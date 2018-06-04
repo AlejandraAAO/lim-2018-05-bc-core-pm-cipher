a9ec79b5ebcc35919db8e516926c96eeeabf7994
AlejandraAAO <aventurero_7_20@hotmail.com>
2018-06-04 13:51:34 -0500

    terminado menos el readme

--- src/cipher.js
+++ src/cipher.js
@@ -1,63 +1,77 @@
-
-//src/cipher.js: acá debes implementar el objeto cipher, el cual debe estar
-// exportado en el objeto global (window). Este objeto (cipher) debe contener dos métodos:
-//cipher.encode(offset, string): offset es el número de posiciones que 
+//src/cipher.js: acÃ¡ debes implementar el objeto cipher, el cual debe estar
+// exportado en el objeto global (window). Este objeto (cipher) debe contener dos mÃ©todos:
+//cipher.encode(offset, string): offset es el nÃºmero de posiciones que 
 //queremos mover a la derecha en el alfabeto y string el mensaje (texto) que queremos cifrar.
-//cipher.decode(offset, string): offset es el número de posiciones que 
+//cipher.decode(offset, string): offset es el nÃºmero de posiciones que 
 //queremos mover a la izquierda en el alfabeto y string el mensaje (texto) que queremos decifrar
 
-//window.cipher = {
-  // poniendo objetos
-  //encodificar: function encode (offset, string)
-  //decodificar:function
-//};
-
-
-
-//var ingresoTextEn = document.getElementById("encripMes");// el texto encriptado /valor=ingresoTextEn.value
-//var moveEn = document.getElementById("leftMove");  //desplazamiento o offset /valor=moveDe.value
-
-function encode(ingresoTextEn,moveEn){ //agarra el valor ingresado de texto y desplaamiento
-	var textDecodificado = [];
-
-//  alert("ingresos: " + ingresoTextEn.value + "  " + moveEn.value);
-
-//  alert("longitud var: " + ingresoTextEn.value.length);
-
-	for(var i = 0; i < ingresoTextEn.value.length; i++){
-		var mayus = ingresoTextEn.value.toUpperCase();
-		var asi = mayus.charCodeAt(i);
-		var asidos = (asi - 65 + moveEn.value) % 26 + 65;
-		textDecodificado.push(String.fromCharCode(asidos));
-		}
-		
-  //alert("arreglo: " + (textDecodificado.join("")));		
-		
-	return textDecodificado.join("");
-
-	}
-//console.log(encode(A,2));
-
-//--------------------------------------
-//-------------------------------------Decodificando
-
-function decode(ingresoTextEn,moveDe){ //agarra el valor ingresado de texto y desplaamiento
-	var textDecodificado = [];
-
-//  alert("ingresos: " + ingresoTextEn.value + "  " + moveEn.value);
-
-//  alert("longitud var: " + ingresoTextEn.value.length);
-
-	for(var i = 0; i < ingresoTextEn.value.length; i++){
-		var mayus = ingresoTextEn.value.toUpperCase();
-		var asi = mayus.charCodeAt(i);
-		var asidos = (asi - 65 - moveEn.value) % 26 + 65;
-		textDecodificado.push(String.fromCharCode(asidos));
-		}
-		
-  //alert("arreglo: " + (textDecodificado.join("")));		
-		
-	return textDecodificado.join("");
-
-	}
-
+window.cipher = {
+      // poniendo objetos
+          encode : function(string,offset){
+          var textDecodificado = []; //array contenedor
+          
+                for (let i=0; i<string.length; i++){
+                      var ascii =  string[i].charCodeAt();
+                            if (ascii >=65 && ascii <=90) { //para las mayusculas!!!
+                      
+                      //console.log(ascii)
+                                  var  encodeNum = ((ascii - 65) + parseInt(offset)) % 26 + 65;
+                      //console.log(encodeNum)         
+                                  textDecodificado.push(String.fromCharCode(encodeNum));
+                                  
+                            } else if (ascii >=97 && ascii <=122){ //para las minunculas
+                           //console.log(ascii)
+                                   encodeNum = ((ascii - 97) + parseInt(offset)) % 26 + 97;
+                                  textDecodificado.push(String.fromCharCode(encodeNum));
+                                  
+                            }else { //espacio
+                                  textDecodificado.push(string[i]);
+                }
+                }
+          return textDecodificado.join("")
+          },
+     //console.log(encode("B z",2));
+     //console.log(encode("A z",2));
+     //console.log(encode("A z",2));
+     //----------------------------DECODIFICANDO----------
+          decode : function(string,offset){
+          var textDecodificado = []; //array contenedor
+          
+                for (let i=0; i<string.length; i++){
+                      var ascii =  string[i].charCodeAt();
+                            if (ascii >=65 && ascii <=90) { //para las mayusculas!!!
+               //console.log(ascii)
+                                  var cal1 = ((ascii - 65) - parseInt(offset)) % 26;
+    
+                //alert("ascii : " + ascii + " . " + (ascii - 65) + " - " + cal1)
+                            
+                                        if (cal1 < 0) {
+                                              cal1  = cal1 + 26;
+                                              var decodeNum = cal1 + 65;
+                                        }else{	
+                                              decodeNum = ((ascii - 65) - parseInt(offset)) % 26 + 65;
+                }
+                //console.log(decodeNum)
+                                  textDecodificado.push(String.fromCharCode(decodeNum));
+                                  
+                            } else if (ascii >=97 && ascii <=122){ //para las minunculas
+                                  //console.log(ascii)
+                                   cal1 = ((ascii - 97) - parseInt(offset)) % 26;
+                                  //alert("ascii : " + ascii + " . " + (ascii - 97) + " - " + cal1)
+                                        if (cal1 < 0) {
+                      cal1  = cal1 + 26;
+                                               decodeNum = cal1 + 97;
+                                        }else{	
+                                              decodeNum = ((ascii - 97) - parseInt(offset)) % 26 + 97;
+                                        }
+                                  textDecodificado.push(String.fromCharCode(decodeNum));
+                                  
+                            }else { //espacio
+                                  textDecodificado.push(string[i]);
+                            }
+                }
+          return textDecodificado.join("")
+          },
+     //console.log(decode("C Z",2));
+    };
+    
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 //acá debes escuchar eventos del DOM, invocar cipher.encode() o cipher.decode() según sea necesario y actualizar el resultado en la UI.
 
 //agarramos los elementos del lado de encriptar
-var ingresoText = document.getElementById("normalMes");
+var ingresoText = document.getElementById("normalMess");
 var moveEn = document.getElementById("rightMove");
 const buttomEn = document.getElementById("butEn"); //aqui
 var resultEn = document.getElementById("outputEn");
@@ -9,43 +9,31 @@ var resultEn = document.getElementById("outputEn");
 
 //agregamos un escuchador de eventos a el textarea de encodificar y me devuelve lo que escribo 
 
-ingresoText.addEventListener("input", ()=>{
+/*ingresoText.addEventListener("input", ()=>{
 	console.log(ingresoText.value);
 	})
-	
-	
+		
 ingresoText.addEventListener("input", ()=>{
 	console.log(moveEn.value);
 	})
-	
+*/	
 	
 
  //agrego otro escuchador de eventos al boton = cuanta las veces que hago click
  //resultEn.innerHTML = (ingresoText.value) si funciona actualiza con el mensaje :)
 buttomEn.addEventListener("click", ()=>{
-	//resultEn.innerHTML = (ingresoText.value) 
-	//console.log(ingresoText.value);
-
-// para ver si llegan los valores a la funcion
-//  alert("ingresos: " + ingresoText.value + "  " + moveEn.value);
+	var letter = ingresoText.value;
+	var numbers = moveEn.value;
+	var messEncod = cipher.encode(letter,numbers);
   
-	resultEn.innerHTML = encode(ingresoText,moveEn);
+	resultEn.innerHTML = messEncod;
 	})
-
-
-
-
-
-
-
 
 
 //buttomEn. onclick = function() {
 	
  //al hacer click en el boton el texto ingresa se muestre en el p con variable  resultEn
 //--------------------------------------------------------------------------------------------------
-
-
 //agarramos los elementod del lado para deencriptar
 var ingresoTextEn = document.getElementById("encripMes");
 var moveDe = document.getElementById("leftMove");
@@ -53,19 +41,19 @@ const resultDe = document.getElementById("outputDe");
 var buttomDe = document.getElementById("butDe");
 
 
-ingresoTextEn.addEventListener("input", ()=>{
+/*ingresoTextEn.addEventListener("input", ()=>{
 	console.log(ingresoTextEn.value);
 	})
 	
 ingresoTextEn.addEventListener("input", ()=>{
 	console.log(moveDe.value);
 	})	
-	
+*/	
  //agrego otro escuchador de eventos al boton = cuanta las veces que hago click
  //resultDe.innerHTML = (ingresoTextEn.value) si funciona actualiza con el mensaje :)
 buttomDe.addEventListener("click", ()=>{
-	//resultDe.innerHTML = (ingresoTextEn.value) 
-	//console.log(ingresoTextEn.value);
-	//})
-	resultDe.innerHTML = decode(ingresoTextEn,moveDe);
+	var letter2 = ingresoTextEn.value;
+	var numbers2 = moveDe.value;
+	var messDecod = cipher.decode(letter2,numbers2);
+	resultDe.innerHTML = messDecod;
 })
